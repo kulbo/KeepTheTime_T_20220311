8b586e51e6bd332d273e6d7a8aef6032c5689418
이 성 노 <kulbo0926@gmail.com>
2022-03-13 11:38:52 +0900

    로딩화면 추가

---
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/SplashActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.smartsoft.keepthetime_t_20220311
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
