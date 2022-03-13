b966bc3c974ad6e1794a99d83c8320aaaefd324a
이 성 노 <kulbo0926@gmail.com>
2022-03-13 14:49:08 +0900

    로그인 함수 호출 > 서버 기능 Call  - 임시 Callback 등록 : 응답 내용만 확인

--- app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/SignInActivity.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/SignInActivity.kt
@@ -2,10 +2,15 @@ package kr.co.smartsoft.keepthetime_t_20220311
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.keepthetime_t_20220311.api.APIList
 import kr.co.smartsoft.keepthetime_t_20220311.api.ServerAPI
 import kr.co.smartsoft.keepthetime_t_20220311.databinding.ActivitySignInBinding
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignInActivity : BaseActivity() {
     lateinit var binding : ActivitySignInBinding
@@ -19,7 +24,24 @@ class SignInActivity : BaseActivity() {
 
     override fun setUpEvents() {
 
+        binding.btnLogin.setOnClickListener {
 
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPassword = binding.edtPassword.text.toString()
+
+            apiList.postRequestLogin(inputEmail, inputPassword).enqueue(object : Callback<JSONObject> {
+                override fun onResponse(call: Call<JSONObject>, response: Response<JSONObject>) {
+
+                    Log.d("응답확인", response.toString())
+                }
+
+                override fun onFailure(call: Call<JSONObject>, t: Throwable) {
+//                    서버에 물리적 연결 실패.
+                }
+
+            })
+
+        }
     }
 
     override fun setValues() {
