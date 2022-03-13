fdef1497b2e02dc9f74c2786c9711f3e79b62ad7
이 성 노 <kulbo0926@gmail.com>
2022-03-13 11:45:03 +0900

    스프레쉬 2.5초 이동

--- app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/SplashActivity.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/SplashActivity.kt
@@ -1,11 +1,33 @@
 package kr.co.smartsoft.keepthetime_t_20220311
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+//      임시 - 2.5초 후에 무조건 로그인
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed( {
+            val myIntent = Intent(mContext, SignInActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+        }, 2500)
+
+
     }
 }
