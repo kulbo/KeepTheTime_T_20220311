e67d8ad31f775810a3bc02aa75b2ba70b7229c2a
이 성 노 <kulbo0926@gmail.com>
2022-03-14 10:00:17 +0900

    내정보 조회 API  - Header  - GET 메쏘드 활용

--- app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/MainActivity.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/MainActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.keepthetime_t_20220311.databinding.ActivityMainBinding
+import kr.co.smartsoft.keepthetime_t_20220311.datas.BasicResponse
+import kr.co.smartsoft.keepthetime_t_20220311.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
     lateinit var binding: ActivityMainBinding
@@ -23,5 +28,16 @@ class MainActivity : BaseActivity() {
 
 //        GET - /user 접근해서 내 정보 조회
 //        토큰값이 필요함. =>
+
+        apiList.getRequestMyInfo(ContextUtil.getLoginUserToken(mContext)).enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
     }
 }
