5c8e399b53d136b725e75d6d8f4f7c35c1e46fcd
이 성 노 <kulbo0926@gmail.com>
2022-03-14 11:58:49 +0900

    로그아웃 UI 그리기

--- app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/SplashActivity.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/SplashActivity.kt
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import kr.co.smartsoft.keepthetime_t_20220311.datas.BasicResponse
+import kr.co.smartsoft.keepthetime_t_20220311.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +25,33 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
 
+        var isMyInfoLoaded = false
+
+        apiList.getRequestMyInfo(ContextUtil.getLoginUserToken(mContext)).enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if (response.isSuccessful) {
+
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
 //      임시 - 2.5초 후에 무조건 로그인
         val myHandler = Handler(Looper.getMainLooper())
 
         myHandler.postDelayed( {
-            val myIntent = Intent(mContext, SignInActivity::class.java)
+            val myIntent : Intent
+
+            if(isMyInfoLoaded) {
+                myIntent = Intent(mContext, MainActivity::class.java)
+            } else{
+                myIntent = Intent(mContext, MainActivity::class.java)
+            }
             startActivity(myIntent)
 
-            finish()
         }, 2500)
 
 
