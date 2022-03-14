786bcd2fb38333763d413be2d55792fbb4e6d0b1
이 성 노 <kulbo0926@gmail.com>
2022-03-14 11:15:01 +0900

    내 프로필 프래그먼트의 데이터 바인딩 세팅

--- app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/fragments/MyProfileFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import kr.co.smartsoft.keepthetime_t_20220311.R
+import kr.co.smartsoft.keepthetime_t_20220311.databinding.FragmentMyProfileBinding
 import kr.co.smartsoft.keepthetime_t_20220311.datas.BasicResponse
 import kr.co.smartsoft.keepthetime_t_20220311.utils.ContextUtil
 import retrofit2.Call
@@ -13,12 +15,16 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MyProfileFragment : BaseFragment() {
+
+    lateinit var binding: FragmentMyProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_profile, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -35,7 +41,11 @@ class MyProfileFragment : BaseFragment() {
 //        내 정보 조회 > UI 조회
         apiList.getRequestMyInfo( ContextUtil.getLoginUserToken(mContext) ).enqueue(object :Callback<BasicResponse>{
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if (response.isSuccessful) {
+                    val br = response.body()!!
 
+//                    =br.data.user.nick_name       // 프레그먼트의 txtNickname을 가져와야 하나?
+                }
             }
 
             override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
