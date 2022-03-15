ee9419b7ea8a7e9db12378399809dc5812e8a3f2
이 성 노 <kulbo0926@gmail.com>
2022-03-15 15:41:51 +0900

    날짜 선택창 구현

--- app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/EditAppointmentActivity.kt
@@ -1,15 +1,20 @@
 package kr.co.smartsoft.keepthetime_t_20220311
 
 import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.keepthetime_t_20220311.databinding.ActivityEditAppointmentBinding
+import java.text.SimpleDateFormat
 
 class EditAppointmentActivity : BaseActivity() {
     lateinit var binding : ActivityEditAppointmentBinding
+
+//
+    val mSelectedAppointmentDateTime = java.util.Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +27,23 @@ class EditAppointmentActivity : BaseActivity() {
         binding.txtDate.setOnClickListener {
             val dsl = object : DatePickerDialog.OnDateSetListener{
                 override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+//                    연/월/일은 JAVA/Kotlin 언어의 기준(0~11월)
                     Toast.makeText(mContext, "${year}년 ${month}월 ${dayOfMonth}일 선택함", Toast.LENGTH_SHORT).show()
+
+                    mSelectedAppointmentDateTime.set(year, month, dayOfMonth)
+
+                    val sdf = SimpleDateFormat("yy/MM/dd")
+
+                    binding.txtDate.text = sdf.format(mSelectedAppointmentDateTime)
                 }
 
             }
             val dpd = DatePickerDialog(
                 mContext,
                 dsl,
-                2022,
-                3,
-                15
+                mSelectedAppointmentDateTime.get(Calendar.YEAR),
+                mSelectedAppointmentDateTime.get(Calendar.MONTH),
+                mSelectedAppointmentDateTime.get(Calendar.DAY_OF_MONTH)
             ).show()
         }
 
--- app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/fragments/AppointmentListFragment.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/fragments/AppointmentListFragment.kt
@@ -1,10 +1,12 @@
 package kr.co.smartsoft.keepthetime_t_20220311.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.keepthetime_t_20220311.EditAppointmentActivity
 import kr.co.smartsoft.keepthetime_t_20220311.R
 import kr.co.smartsoft.keepthetime_t_20220311.databinding.FragmentAppointmentListBinding
 
@@ -23,10 +25,15 @@ class AppointmentListFragment : BaseFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
-
+        binding.btnAddAppointment.setOnClickListener {
+            val myIntent = Intent(mContext, EditAppointmentActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 
     override fun setValues() {
