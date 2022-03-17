d7cb186b4cca953713ccb6829049ec119cceee57
이 성 노 <kulbo0926@gmail.com>
2022-03-17 22:54:38 +0900

    장소데이터 클래스 생성까지

--- app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/ManagePlacesActivity.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/ManagePlacesActivity.kt
@@ -1,6 +1,5 @@
 package kr.co.smartsoft.keepthetime_t_20220311
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.keepthetime_t_20220311.databinding.ActivityManagePlacesBinding
