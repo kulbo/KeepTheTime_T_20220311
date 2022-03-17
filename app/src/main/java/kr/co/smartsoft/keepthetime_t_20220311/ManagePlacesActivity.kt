71cf768903602d677fac4069823a7db518972f5a
이 성 노 <kulbo0926@gmail.com>
2022-03-17 22:50:29 +0900

    장소데이터 클래스 생성까지

---
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/ManagePlacesActivity.kt
@@ -0,0 +1,25 @@
+package kr.co.smartsoft.keepthetime_t_20220311
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.keepthetime_t_20220311.databinding.ActivityManagePlacesBinding
+
+class ManagePlacesActivity : BaseActivity() {
+    lateinit var binding:ActivityManagePlacesBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_places)
+        setUpEvents()
+        setValues()
+    }
+
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
