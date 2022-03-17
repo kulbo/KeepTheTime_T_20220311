7cbd8c36872dc34c7c5c1197dafe67daa7ab6c16
이 성 노 <kulbo0926@gmail.com>
2022-03-18 06:04:04 +0900

    내 장소 목록 조회 API 호출

--- app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/ManagePlacesActivity.kt
+++ app/src/main/java/kr/co/smartsoft/keepthetime_t_20220311/ManagePlacesActivity.kt
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.keepthetime_t_20220311.databinding.ActivityManagePlacesBinding
+import kr.co.smartsoft.keepthetime_t_20220311.datas.BasicResponse
+import kr.co.smartsoft.keepthetime_t_20220311.datas.PlaceData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ManagePlacesActivity : BaseActivity() {
     lateinit var binding:ActivityManagePlacesBinding
+
+    val mPlaceList = ArrayList<PlaceData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +28,23 @@ class ManagePlacesActivity : BaseActivity() {
 
     override fun setValues() {
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getMyPlacesFromServer()
+    }
+
+    fun getMyPlacesFromServer() {
+        apiList.getRequestMyPlaceList().enqueue(object :Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
     }
 }
